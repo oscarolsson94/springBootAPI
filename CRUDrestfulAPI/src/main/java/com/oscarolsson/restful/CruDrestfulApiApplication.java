b0103ef8070ae3d63b625b65978f2ac87b4aadc6
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-03-10 08:33:31 +0100

    spring boot proj setup

---
+++ CRUDrestfulAPI/src/main/java/com/oscarolsson/restful/CruDrestfulApiApplication.java
@@ -0,0 +1,13 @@
+package com.oscarolsson.restful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CruDrestfulApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruDrestfulApiApplication.class, args);
+	}
+
+}
---
+++ CRUDrestfulAPI/src/test/java/com/oscarolsson/restful/CruDrestfulApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oscarolsson.restful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruDrestfulApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
