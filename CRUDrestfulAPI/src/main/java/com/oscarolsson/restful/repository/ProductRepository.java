269a3187a7e569b36b5878527595652a6c9abd99
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-03-10 08:53:04 +0100

    app properties

---
+++ CRUDrestfulAPI/src/main/java/com/oscarolsson/restful/repository/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.oscarolsson.restful.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.oscarolsson.restful.entity.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Integer>{ //Name of entity and type of Primary key
+
+	
+	
+}
