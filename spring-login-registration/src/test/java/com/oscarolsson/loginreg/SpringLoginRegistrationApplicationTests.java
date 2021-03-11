13ca13abeeb369b2617d92563f974034d65dd7cc
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-03-11 10:26:27 +0100

    project setup

---
+++ spring-login-registration/src/test/java/com/oscarolsson/loginreg/SpringLoginRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oscarolsson.loginreg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLoginRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
