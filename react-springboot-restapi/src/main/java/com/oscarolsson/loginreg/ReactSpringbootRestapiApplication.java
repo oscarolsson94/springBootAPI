53e9f1c8aff3a81e4dc28c8ab152ff858d6837e7
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-03-11 17:53:25 +0100

    project setup

---
+++ react-springboot-restapi/src/main/java/com/oscarolsson/loginreg/ReactSpringbootRestapiApplication.java
@@ -0,0 +1,13 @@
+package com.oscarolsson.loginreg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactSpringbootRestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactSpringbootRestapiApplication.class, args);
+	}
+
+}
