414dc461a4319ea2c21691efec94d7e2cb468d0a
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-03-11 11:16:32 +0100

    user repo

---
+++ spring-login-registration/src/main/java/com/oscarolsson/loginreg/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.oscarolsson.loginreg.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.oscarolsson.loginreg.model.User;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{ //class type of entity and primary key
+
+}
---
+++ spring-login-registration/src/main/java/com/oscarolsson/loginreg/service/UserService.java
@@ -0,0 +1,5 @@
+package com.oscarolsson.loginreg.service;
+
+public interface UserService {
+
+}
