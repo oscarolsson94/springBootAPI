962e43f14d29c82e5bd34d0aa81ea0125a62ba8b
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-03-12 15:27:05 +0100

    enable cross-domain communication

--- react-springboot-fullstack-backend/src/main/java/com/oscarolsson/springboot/controller/EmployeeController.java
+++ react-springboot-fullstack-backend/src/main/java/com/oscarolsson/springboot/controller/EmployeeController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.oscarolsson.springboot.model.Employee;
 import com.oscarolsson.springboot.repository.EmployeeRepository;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:3000")		//enable cross-domain communication
 @RestController
 @RequestMapping("/api/v1")
 public class EmployeeController {
