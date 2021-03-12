192138cad77b1afe55712f2a88fc1fde4e936676
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-03-12 15:53:30 +0100

    some tweaking and css

--- react-springboot-fullstack-frontend/src/App.js
+++ react-springboot-fullstack-frontend/src/App.js
@@ -2,14 +2,17 @@ import React from 'react';
 import ListEmployeeComponent from './components/ListEmployeeComponent';
 import HeaderComponent from './components/HeaderComponent';
 import FooterComponent from './components/FooterComponent';
+import './App.css';
 
 function App() {
-  return (
-      <div className="container">
-          <HeaderComponent />
-          <ListEmployeeComponent />
-          <FooterComponent />
-    </div>
+    return (
+        <div>
+            <HeaderComponent />
+                 <div className="container">        
+                    <ListEmployeeComponent />
+                 </div>
+            <FooterComponent />
+        </div>
   );
 }
 
--- react-springboot-fullstack-frontend/src/components/HeaderComponent.js
+++ react-springboot-fullstack-frontend/src/components/HeaderComponent.js
@@ -5,7 +5,7 @@ const HeaderComponent = () => {
         <div>
             <header>
                 <nav className="navbar navbar-expand-md navbar-dark bg-dark">
-                    <div><a href="#" className="navbar-brand">Employee Management App</a></div>
+                    <div><a href="/" className="navbar-brand">Employee Management App</a></div>
                 </nav>
             </header>
         </div>
