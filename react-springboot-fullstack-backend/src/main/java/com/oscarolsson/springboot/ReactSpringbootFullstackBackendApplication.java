384836c2eb218af2bc76ea01fe5da889b063026f
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-03-12 10:55:18 +0100

    structure setup and properties

---
+++ react-springboot-fullstack-backend/src/main/java/com/oscarolsson/springboot/ReactSpringbootFullstackBackendApplication.java
@@ -0,0 +1,13 @@
+package com.oscarolsson.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactSpringbootFullstackBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactSpringbootFullstackBackendApplication.class, args);
+	}
+
+}
---
+++ react-springboot-fullstack-backend/src/test/java/com/oscarolsson/springboot/ReactSpringbootFullstackBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oscarolsson.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactSpringbootFullstackBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
