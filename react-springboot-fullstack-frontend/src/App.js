b269c95e1d5f61dfcaa6c4ee4b11505c34850d56
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-03-12 15:39:46 +0100

    header and footer

--- react-springboot-fullstack-frontend/src/App.js
+++ react-springboot-fullstack-frontend/src/App.js
@@ -1,10 +1,14 @@
-import React from 'react'
-import ListEmployeeComponent from './components/ListEmployeeComponent'
+import React from 'react';
+import ListEmployeeComponent from './components/ListEmployeeComponent';
+import HeaderComponent from './components/HeaderComponent';
+import FooterComponent from './components/FooterComponent';
 
 function App() {
   return (
-    <div className="container">
-        <ListEmployeeComponent />
+      <div className="container">
+          <HeaderComponent />
+          <ListEmployeeComponent />
+          <FooterComponent />
     </div>
   );
 }
